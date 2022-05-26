ebdb9c327359f7bdd68bc3795040a87c872357c5
igorcn <igorcanunes@gmail.com>
2022-05-26 13:54:47 -0300

    primeira entrega

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './Home.css';
+
+function Home(){
+    return (
+        <>
+        <h1 className='titulo'> Home </h1>
+        <img src="https://edm2.b-cdn.net/blog/wp-content/uploads/2021/12/pixelart-480x480.jpg" alt="primeira imagem" className='img' />
+
+        </>
+    );
+}
+
+export default Home;
